3613f20455741df640f371c3c11856d454a2fa33
Gabriel Nascimento de Oliveira <gndeoliveira@outlook.com>
2022-05-09 22:21:05 -0300

    Atividade Fiap - Greenfield Guitars

---
+++ script.js
@@ -0,0 +1,56 @@
+const getData = () => {
+    $.ajax({
+        url: "./assets/lista.json",
+        data: "",
+        success: function (retorno) {
+            processData(retorno);
+        },
+        dataType: "html"
+    });
+}
+
+const processData = (data) => {
+    const myData = JSON.parse(data);
+
+    const myTable = $('.myTable');
+
+    $(myData).each((index, value) => {
+        let content = `
+        <tr data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="showModal('${value.model}','${value.type}','${value.img}','${value.link}')">
+            <th scope="row">${value.id}</th>
+            <td>${value.model}</td>
+            <td>${value.type}</td>
+            <td><img class="images" src="${value.img}" alt=""></td>
+        </tr>
+        `;
+
+        myTable.append(content);
+    });
+}
+
+const showModal = (model, type, img, link) => {
+    let content = `
+    <div class="col-md-12">
+        <div class="card" >
+            <img src="${img}" class="card-img-top" alt="text">
+            <div class="card-body">
+                <h5 class="card-title">${model}</h5>
+                <p class="card-text">${type}</p>
+            </div>
+        </div>
+    </div>`;
+
+    const cardFooter = $('.modal-footer');
+    let myButtons = `
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <a href="${link}" target="_blank"><button type="button" class="btn btn-primary">More Info</button></a>
+        `;
+    cardFooter.html(myButtons);
+
+    $('.detail').html(content);
+    $('#exampleModal').modal('show');
+}
+
+$(() => {
+    getData();
+})
